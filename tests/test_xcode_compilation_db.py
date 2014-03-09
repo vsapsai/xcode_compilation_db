75a78f85dd72f0ca6549546483526e3ddf81e32d
Volodymyr Sapsai <vsapsai@gmail.com>
2014-03-09 22:53:57 +0200

    Make tests less fragile.
    Xcode stores intermediate files in directories with randomized names.  But
    directory names aren't completely random, only suffix is random.  So I detect
    this random part and check that it's the same for all files in Xcode project.

--- tests/test_xcode_compilation_db.py
+++ tests/test_xcode_compilation_db.py
@@ -9,6 +9,17 @@ import unittest
 
 DB_FILENAME = "compile_commands.json"
 
+def first_different_character_index(str1, str2):
+    """Returns index of the first character which differs in str1 and str2."""
+    min_length = min(len(str1), len(str2))
+    for i in range(min_length):
+        if str1[i] != str2[i]:
+            return i
+    if len(str1) != len(str2):
+        return min_length
+    return -1
+
+
 class CompilationDatabaseRegressionTestCase(unittest.TestCase):
     def test(self):
         test_directory = os.path.dirname(os.path.abspath(__file__))
@@ -37,13 +48,28 @@ class CompilationDatabaseRegressionTestCase(unittest.TestCase):
     def assert_equal_compilation_db(self, expected_db, actual_db, project_directory):
         self.assertEqual(len(expected_db), len(actual_db), "Length should be the same")
         actual_db_dict = {record["file"]: record for record in actual_db}
+        random_dir_part = None
         assert len(actual_db_dict) == len(actual_db), "Expect no file duplicates"
         for expected_record in expected_db:
             actual_record = actual_db_dict[expected_record["file"]]
             self.assertEqual(set(expected_record.keys()), set(actual_record.keys()), "Keys should be equal")
             for key, value in expected_record.iteritems():
                 value = value.replace("{{PROJECT_DIR}}", project_directory)
+                if key == "command":
+                    if random_dir_part is None:
+                        random_dir_part = self.detect_random_dir_part(value, actual_record[key])
+                        self.assertIsNotNone(random_dir_part, "Failed to detect random directory part")
+                    value = value.replace("{{RANDOM_DIR_PART}}", random_dir_part)
                 self.assertEqual(value, actual_record[key])
+
+    def detect_random_dir_part(self, expected_str, actual_str):
+        different_char_i = first_different_character_index(expected_str, actual_str)
+        if different_char_i == -1:
+            return None
+        if not expected_str[different_char_i:].startswith("{{RANDOM_DIR_PART}}"):
+            return None
+        random_part = actual_str[different_char_i : different_char_i+28]
+        return random_part
 
 if __name__ == "__main__":
     unittest.main()
